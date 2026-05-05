2e37007081a2c98febde325610f0f95f0a6b4ae0
John-Natty <jonathanmerle8426@gmail.com>
2026-05-05 16:33:39 +0200

    fix: correct import statement in variable_load.py

--- python_fundamentals/functions_modules/variable_load.py
+++ python_fundamentals/functions_modules/variable_load.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Program that imports a variable."""
 
-from variable_load_5.py import a
+from variable_load_5 import a
 
 if __name__ == "__main__":
     print("{}".format(a))
