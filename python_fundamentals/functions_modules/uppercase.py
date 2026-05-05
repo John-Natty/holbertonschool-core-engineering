573a76586c5cb7d14422e68c3ab089ba1e6a7743
John-Natty <jonathanmerle8426@gmail.com>
2026-05-05 15:01:28 +0200

    update uppercase.py to change docstring and print result instead of returning it

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 def uppercase(str):
-    """Return the uppercase version of a string."""
+    """Print a string in uppercase."""
     result = ""
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             result += chr(ord(c) - 32)
         else:
             result += c
-    return result
+    print(result)
