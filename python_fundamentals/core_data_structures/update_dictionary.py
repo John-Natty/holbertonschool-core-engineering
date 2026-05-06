c17e656521c418e97f8ae4c12c44641564d42f20
John-Natty <jonathanmerle8426@gmail.com>
2026-05-06 15:52:56 +0200

    add: implement update_dictionary function to update or add key/value pairs in a dictionary

---
+++ python_fundamentals/core_data_structures/update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Update or add a key/value in a dictionary."""
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Update or add a key/value pair in a dictionary."""
+    a_dictionary[key] = value
+    return a_dictionary
