ccaa9c7990fb082a32cba72bf4a3ac1488db138d
John-Natty <jonathanmerle8426@gmail.com>
2026-05-11 15:09:25 +0200

    add: create 4-square.py to define Square class with size validation and area calculation

---
+++ python_oop/classes_and_object_model/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """A class that defines a square."""
+    def __init__(self, size=0):
+        """Initialize Square with size validation."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculate and return the area of the square."""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
