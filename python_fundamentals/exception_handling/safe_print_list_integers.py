ea44e76eb2657fd93d468323a66ea92d1de6cd90
John-Natty <jonathanmerle8426@gmail.com>
2026-05-11 10:45:43 +0200

    add: implement safe_print_list_integers function to print specified number of integer elements from a list

---
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    """Print x elements of a list that are integers.
+
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements to print.
+
+    Returns:
+        int: The number of integers printed.
+    """
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+        except IndexError:
+            break
+    print()
+    return count
