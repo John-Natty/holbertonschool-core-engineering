981d91d8c3bdb446bc223b5d736a67fe8fa3a752
John-Natty <jonathanmerle8426@gmail.com>
2026-05-11 11:04:59 +0200

    add: create raise_exception_msg function to raise a NameError exception with a custom message

---
+++ python_fundamentals/exception_handling/raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a custom message.
+
+    Args:
+        message (str): The message to include in the exception.
+    """
+    raise NameError(message)
