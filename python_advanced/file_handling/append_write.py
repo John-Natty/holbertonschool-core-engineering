b1069e1a48548a0920bdaaaeb97110ad1b3f84fb
John-Natty <jonathanmerle8426@gmail.com>
2026-05-25 14:45:53 +0200

    refactor: improve docstring in append_write function for clarity and completeness

--- python_advanced/file_handling/append_write.py
+++ python_advanced/file_handling/append_write.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+"""Module for appending text to a file."""
 
 
 def append_write(filename="", text=""):
-    """Append a string to a UTF-8 text file"""
-    # Open the file in append mode with UTF-8 encoding
+    """Append a string to a UTF-8 text file.
+
+    Return the number of characters added.
+    """
+    # Open the file in append mode.
+    # Mode "a" creates the file if needed and writes at the end.
     with open(filename, "a", encoding="utf-8") as file:
-        """returns the number of characters added to the file"""
-        # Write the text to the file and get the number of characters written
+        # write() appends the text and returns the number of characters written
         return file.write(text)
